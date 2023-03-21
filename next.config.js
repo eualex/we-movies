fbb2433968a1bae6c183cc516c03aebc8f14afe0
alex.matos <alex.matos@ceuma.br>
2023-03-21 02:58:25 -0300

    fix: corrigindo erro do styled component no next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     domains: ['wefit-react-web-test.s3.amazonaws.com'],
   },
